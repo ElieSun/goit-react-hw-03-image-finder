c6ee7856a35763114f16efcfdc8bec7cd947311c
ElieSun <elinavoloshyk@gmail.com>
2023-01-29 20:38:31 +0200

    start pr

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Searchbar } from "./Searchbar/Searchbar";
+import { Component } from "react";
+import { fetchImages } from 'services/apiServices';
+
+export class App extends Component {
+
+  state = {
+    query: '',
+    images: [],
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    fetchImages(); 
+  }
+
+  handleSubmit =  (query) => {
+    this.setState({ query });
+  }
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.handleSubmit}/>
+      </> 
+    )
+  }
+}
